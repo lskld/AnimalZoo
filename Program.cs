e9076e893fd9d7b2b60e10f75edae39960374a68
lskld <theo.lillieskold@gmail.com>
2025-09-19 16:12:06 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿namespace AnimalZoo
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            
+        }
+
+        class Animal()
+        {
+            /* 
+            * Space for 5 common Animal properties
+            * 
+            * 
+            * 
+            * 
+            */
+            
+            static void MakeSound()
+            {
+
+            } 
+
+            static void Eat()
+            {
+
+            }
+
+            static void Sleep()
+            {
+
+            }
+
+        }
+
+        class Dog: Animal
+        {
+
+            static void Sit()
+            {
+
+            }
+        }
+
+        class Cat: Animal
+        {           
+
+            static void Attack()
+            {
+
+            }
+        }
+
+        class Bird: Animal
+        {
+
+            static void Fly ()
+            {
+
+            }
+        }
+
+    }
+}
